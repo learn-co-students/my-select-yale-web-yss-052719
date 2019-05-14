3b94235bdd59a9ba1dc38b27bf81fcf41e254785
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-14 02:01:53 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+ i=0
+ return_array = Array.new
+ while i<collection.length do
+   new_value = yield collection[i]
+   if new_value == true
+     return_array << collection[i]
+   end
+   i +=1
+ end
+ return_array
 end
