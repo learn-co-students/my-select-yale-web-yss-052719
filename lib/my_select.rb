30e4d45a0977702ae484e4c6d2895fbd78da9b6c
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-20 23:33:48 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  counter = 0
+  selected = []
+  while counter < collection.length
+    if yield(collection[counter]) == true
+      new_ = collection[counter]
+      selected << new_
+    else
+    end
+  counter += 1
+  end
+  return selected
 end
